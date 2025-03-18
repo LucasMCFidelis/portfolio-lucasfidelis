de126186f4f772bbfd83cbaf02d6b71cc157cf6a
Lucas Fidelis <lucasm241301@gmail.com>
2025-03-18 16:12:37 -0300

    Adiciona sombra e ajusta para que o header fique fixo no topo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,7 +13,7 @@ export default function Header() {
   ];
 
   return (
-    <header className="w-full flex items-center justify-between p-4">
+    <header className="w-full h-[10vh] flex items-center justify-between sticky top-0 bg-background shadow-md shadow-accent">
       <h1 className="font-title text-3xl">Lucas Fidelis</h1>
 
       {/* Menu Desktop */}
