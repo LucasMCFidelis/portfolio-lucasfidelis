f9613796759b9321379cd5e1a1d6f851ad9f37b9
Lucas Fidelis <lucasm241301@gmail.com>
2025-03-25 22:46:33 -0300

    Adiciona estrutura basica do Forms para testes

--- src/components/sections/SectionContact.tsx
+++ src/components/sections/SectionContact.tsx
@@ -9,21 +9,60 @@ import {
   CardHeader,
   CardTitle,
 } from "../ui/card";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+});
 
 export default function SectionContact() {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
   return (
-    <SectionWrapper>
-      <Card className="bg-transparent border-none w-full lg:w-3/5">
+    <SectionWrapper className="md:flex-row md:flex-nowrap md:justify-between">
+      <Card className="bg-transparent border-none w-full md:w-3/5">
         <CardHeader className="p-0">
-          <CardTitle><h2>Conecte-se a mim</h2></CardTitle>
+          <CardTitle>
+            <h2>Conecte-se a mim</h2>
+          </CardTitle>
           <CardDescription>
             Entre em contato comigo pelo e-mail:
           </CardDescription>
         </CardHeader>
         <CardContent className="p-0">
-          <Button variant={"link"} className="lowercase">lucasm241301@gmail.com</Button>
+          <Button variant={"link"} className="lowercase">
+            lucasm241301@gmail.com
+          </Button>
         </CardContent>
-        <CardFooter className="p-0">
+        <CardFooter className="p-0 space-x-3">
           <a
             href="https://www.linkedin.com/in/lucas-fidelis-778705149"
             target="_blank"
@@ -44,6 +83,32 @@ export default function SectionContact() {
           </a>
         </CardFooter>
       </Card>
+      <div className="flex-1">
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="flex-1 space-y-8"
+          >
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="shadcn" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    This is your public display name.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
+      </div>
     </SectionWrapper>
   );
 }
