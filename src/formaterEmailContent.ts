2b30392e9a52b1b96bc2daa972e89f8382d46ca2
Lucas Fidelis <lucasm241301@gmail.com>
2025-03-29 23:59:20 -0300

    Ajusta para formatar o email e enviar o html formatado para o backend

---
+++ src/formaterEmailContent.ts
@@ -0,0 +1,22 @@
+export function formaterEmailContent(
+  name: string,
+  from: string,
+  message: string
+) {
+  return `
+    <div style="max-width: 600px; margin: 20px auto; background-color: #ffffff; padding: 20px; border-radius: 8px; box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);">
+      <header>
+        <h1 style="text-align: center; color: #EA4F0D; font-size: 24px; margin-bottom: 20px;">Email enviado através do Portfólio</h1>
+      </header>
+      <div style="font-size: 16px; line-height: 1.6; margin: 10px 0;">
+        <p style="font-size: 16px; margin: 10px 0;"><strong style="color: #EA4F0D;">Nome:</strong> ${name}</p>
+        <p style="font-size: 16px; margin: 10px 0;"><strong style="color: #EA4F0D;">E-mail:</strong> ${from}</p>
+        <p style="font-size: 16px; margin: 10px 0;"><strong style="color: #EA4F0D;">Mensagem:</strong></p>
+        <p style="font-size: 16px; margin: 10px 0;">${message}</p>
+      </div>
+      <footer style="text-align: center; font-size: 14px; color: #777; margin-top: 20px;">
+        <p>Lucas Fidelis</p>
+      </footer>
+    </div>
+  `;
+}
--- src/sendEmail.ts
+++ src/sendEmail.ts
@@ -1,12 +1,16 @@
 import axios from "axios";
+import { formaterEmailContent } from "./formaterEmailContent";
 
 export async function sendEmail(name: string, from: string, subject: string, message: string) {
   try {
+    const htmlContent = formaterEmailContent(name, from, message); // Gera o HTML formatado
+
     const response = await axios.post(import.meta.env.VITE_MAIL_API_ROUTE, {
       name,
       from, // E-mail da pessoa que preencheu o formulário (será usado no reply-to)
       subject,
       message,
+      htmlContent,
     });
 
     console.log("E-mail enviado com sucesso:", response.data);
