702470116a449c609b67e90c6fd4bf34d5a21830
Lucas Fidelis <lucasm241301@gmail.com>
2025-04-01 13:50:32 -0300

    Ajusta para rota / redirecionar para /home

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./pages/Home";
 import AboutMe from "./pages/AbouteMe";
 import { appSections } from "./appSections"; 
@@ -6,6 +6,7 @@ import { appSections } from "./appSections";
 export function AppRoutes() {
   return (
     <Routes>
+      <Route path={"/"} element={<Navigate to={appSections.home.href ?? "/home"} replace/>}  />
       <Route path={appSections.home.href} element={<HomePage />} />
       <Route path={appSections.about.href} element={<AboutMe />} />
     </Routes>
