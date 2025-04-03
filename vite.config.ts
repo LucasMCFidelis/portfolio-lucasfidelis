f59a635d659522f934e1279431354948d18cbd22
Lucas Fidelis <lucasm241301@gmail.com>
2025-04-03 16:10:46 -0300

    Ajusta configuração do vite para testar problema de rotas emm produção

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,13 @@ import path from "path"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined, // Evita chunks desnecessários
+      },
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
