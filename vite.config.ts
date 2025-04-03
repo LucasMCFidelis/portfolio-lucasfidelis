2d0c27e85b3ca218ae5491cf0bab8cade5880bef
Lucas Fidelis <lucasm241301@gmail.com>
2025-04-03 18:59:24 -0300

    feat: adicionar arquivo _redirects para configuração de rotas no Render
    Adicionado o arquivo _redirects na pasta public/ para redirecionar todas as requisições para o index.html, permitindo que o React Router gerencie as rotas no lado do cliente. Isso busca resover o problema de erro 404 ao acessar diretamente URLs específicas.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import path from "path"
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,6 +10,14 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks: undefined, // Evita chunks desnecessários
+      },
+    },
+  },
+  preview: {
+    proxy: {
+      "/": {
+        target: "/index.html",
+        rewrite: () => "/index.html",
       },
     },
   },
