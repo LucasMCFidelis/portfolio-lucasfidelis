581bc95b953ce481e547dce1e46f0bdcfea17056
Lucas Fidelis <lucasm241301@gmail.com>
2025-11-05 18:19:33 -0300

    chore: remove unused files after Next migration

--- src/interfaces/ProjectCardProps.ts
+++
@@ -1,8 +0,0 @@
-export interface ProjectCardProps {
-  title: string;
-  description?: string;
-  year: number;
-  area: string;
-  urlList: { name?: string; url: string, icon?: "default" | "link"  }[];
-  images: string[]
-}
